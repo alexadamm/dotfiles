212fbcdabf13827b81a84689a689061dc87f21a0
Alexander Adam <alexanderadamm21@gmail.com>
2024-06-30 12:00:40 +0700

    feat: implement neovim base setting

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,15 @@
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- tab and indentation settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- leader key
+vim.g.mapleader = ' '
